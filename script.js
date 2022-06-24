46df3501fd637ff4b84044e80de9ee9593443b38
Mary Dillon <mary.dillon09@gmail.com>
2022-06-23 18:28:22 -0700

    generates password with some prompts

--- script.js
+++ script.js
@@ -11,4 +11,100 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
+var lowerAlph = "abcdefghijklmnopqrstuvwxyz"
+var upperAlph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var char = "!@#$%^&*()"
+
 generateBtn.addEventListener("click", writePassword);
+
+var generatePassword = function(){
+
+    var passLen = prompt("How many characters would you like your password to have? (Must be between 8 & 128 characters.");
+    var numbers =  confirm("Would you like numbers in your password?");
+    var upper =  confirm("Would you like uppercase letters in your password?");
+    var lower = confirm("Would you like lowercase letters in your password?");
+
+    var password1 = ""
+
+    
+    for(var i=0; i < passLen; i++){
+        if(numbers){
+            var numbersYes = Math.floor(Math.random() * 10);
+        } else{
+            numbersYes = null;
+        }
+
+        if(upper){
+            var upperYes = upperAlph[Math.floor(Math.random() * upperAlph.length)];
+        } else{
+            upperYes = null;
+        }
+
+        if(lower){
+            var lowerYes = lowerAlph[Math.floor(Math.random() * lowerAlph.length)];
+        } else {
+            lowerYes = null;
+        }
+
+        password1 += (numbersYes+upperYes+lowerYes)
+    }
+
+    password2 = ""
+
+    for(var i=0; i < passLen; i++){
+        password2 += password1[Math.floor(Math.random() * password1.length)];
+    }
+
+    return password2
+
+}
+
+
+
+    
+
+
+// return password1
+
+
+// for(var i=0; i < passLen; i++){
+
+
+
+            // if (numbers === true){
+            // return Math.floor(Math.random() * 10) 
+            // }
+
+                        // if (upper === true){
+            // return upperAlph[Math.floor(Math.random() * upperAlph.length)];
+            // }
+
+                //         if (lower === true){
+    //         return lowerAlph[Math.floor(Math.random() * lowerAlph.length)];
+    // }
+
+// var generatePassword = function(){
+
+  
+//     confirm("Would you like numbers in your password?");
+//           if (numbers === true){
+//             return Math.floor(Math.random() * 10) 
+//           }
+  
+
+// var passLen = prompt("How many characters would you like your password to have? (Must be between 8 & 128 characters.")
+//     if(passLen < 8 || passLen > 128){
+//         var passLen = prompt("Must be between 8 & 128. How many characters would you like in your password?")
+//     }
+
+
+// structure
+// ask how many characters - store this number as length
+// ask if they want numbers - store true or false as numbers
+// ask if they want uppercase - store true or false upper
+// ask if they want lowercase - store true or false as lower
+// ask if they want special characters - store true or false as lower
+// for true values values generate random for each variable
+// have it loop with random until character length is hit
+// print password in password area
